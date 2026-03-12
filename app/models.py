433d3240217a91d4ee4403280535b437610d02b8
InnovaDynamics <cncbalneariosc@gmail.com>
2026-03-11 22:39:14 -0300

    Sistema VIP finalizado y listo

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Boolean, DateTime
+from sqlalchemy.sql import func
+from .database import Base
+
+class Entrada(Base):
+    __tablename__ = "entradas"
+    # Esto asegura que los datos VIP vivan separados de tus Pedidos 2.0
+    __table_args__ = {"schema": "sistema_qr"} 
+
+    id = Column(String, primary_key=True, index=True)
+    nombre_ganador = Column(String)
+    usada = Column(Boolean, default=False)
+    fecha_creacion = Column(DateTime(timezone=True), server_default=func.now())
+    fecha_uso = Column(DateTime(timezone=True), nullable=True)
