3b2fe1ff684124cd628a2d74251859f4eb2e0f83
InnovaDynamics <cncbalneariosc@gmail.com>
2026-03-11 23:42:53 -0300

    Conexion IPv4 a Supabase lista

--- app/database.py
+++ app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# CONEXIÓN DIRECTA (Puerto 5432) - Más estable para pruebas locales
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:26228299j.a@db.lnobkltufmayfxvmloxj.supabase.co:5432/postgres"
+# ⚠️ Pon tu contraseña real en lugar de AQUI_TU_CONTRASEÑA
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.lnobkltufmayfxvmloxj:26228299j.a@aws-1-sa-east-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
